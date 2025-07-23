898395d4a16274cd1dc82dccf0cfdf5e68fdf696
ghddbsdl041 <hong.yy041@gmail.com>
2025-07-23 14:09:55 +0900

    design: 디폴트 폰트 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nexon: ['Noto Sans Korean', 'sans-serif'], // 유틸리티용 커스텀 키
+        sans: ['Noto Sans Korean', 'sans-serif'], // Tailwind 기본 sans 대체
+      }
+    },
   },
   plugins: [],
 }
